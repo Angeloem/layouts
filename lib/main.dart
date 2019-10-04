be81d3d22a172ff3a987b603caa788808c6e941c
angeloem <esanga530@gmail.com>
2019-10-04 04:26:32 -0400

    created a fresh new app and now in the icon phase

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 
-class MyAppBar extends StatelessWidget{
-	MyAppBar({this.title});
-	
-	final Widget title;
-	
-	
-	@override
-	Widget build(BuildContext context) {
-		return Container(
-			height: 56.0,
-			padding: const EdgeInsets.symmetric(horizontal: 8.0),
-			decoration: BoxDecoration(color: Colors.lightBlue[500]),
-			
-			child: Row(
-				children: <Widget>[
-					IconButton(
-						icon: Icon(Icons.menu),
-						tooltip: 'Navigation menu',
-						onPressed: null,
-					),
-					Expanded(
-						child: title,
-					),
-					IconButton(
-						icon: Icon(Icons.search),
-						tooltip: 'Search',
-						onPressed: null,
-					)
-				],
-			),
-		);
-	}
-}
-
-class MyScaffold extends StatelessWidget {
-	@override
-	Widget build(BuildContext context) {
-		return Material(
-			child: Column(
-				children: <Widget>[
-					MyAppBar(
-						title: Text(
-							'Example Text',
-							style: Theme.of(context).primaryTextTheme.title,
-						),
-					),
-					Expanded(
-						child: Center(
-							child: Text('Hello World')
-						),
-					)
-				],
-			),
-		);
-	}
-}
-
-void main() {
+void main(){
 	runApp(MaterialApp(
-		title: 'My App',
-		home: MyScaffold(),
-	)
+		title: 'Floating Action Button',
+		home: TutorialHome,
+	));
+}
+
+class TutorialHome extends StatelessWidget {
+	@override
+	Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+		appBar: AppBar(
+			leading: IconButton(
+				icon: Icon(Icons.menu),
+				tooltip: 'Menu',
+				onPressed: null,
+			),
+		),
 	);
+  }
 }
